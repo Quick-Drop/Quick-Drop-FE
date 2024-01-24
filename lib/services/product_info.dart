a200faa918b659d5c2876fafcb4df756e18b1776
Anas <wg12181218@gmail.com>
2024-01-24 16:40:44 +0900

    [FE-03] <FEAT>Item_list Postman 활용하여 렌더링, <Docs>relative Import로 변경하여 상위 디렉토리 conflict 예방

--- lib/services/product_info.dart
+++ lib/services/product_info.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 class ProductInfo {
   final int id;
   final String name;
@@ -12,4 +15,35 @@ class ProductInfo {
     required this.description,
     required this.address,
   });
+
+  factory ProductInfo.fromJson(Map<String, dynamic> json) {
+    if (!json.containsKey('id') ||
+        !json.containsKey('name') ||
+        !json.containsKey('title') ||
+        !json.containsKey('description') ||
+        !json.containsKey('address')) {
+      throw Exception('Invalid JSON data');
+    }
+    return ProductInfo(
+      id: json["id"],
+      name: json["name"],
+      title: json["title"],
+      description: json["description"],
+      address: json["address"],
+    );
+  }
+}
+
+class ItemListApi {
+  static Future<List<ProductInfo>> fetchItemList() async {
+    final response = await http.get(Uri.parse(
+        'https://c0d37d40-3638-49a9-942b-3fb838191686.mock.pstmn.io/list'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = jsonDecode(response.body);
+      return jsonData.map((item) => ProductInfo.fromJson(item)).toList();
+    } else {
+      throw Exception('Failed to load Item List');
+    }
+  }
 }
