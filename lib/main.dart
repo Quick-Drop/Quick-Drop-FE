d3022641331a6d9d3be450b61b99d69f2af50bb7
Chanjoo Park <cjangel0503@gmail.com>
2024-01-27 19:07:00 +0900

    머지 전 커밋

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-=======
 import 'package:provider/provider.dart';
 import 'package:quick_drop/services/product_info_api.dart';
->>>>>>> origin/main
 import 'screen/home/home.dart';
 
 void main() {
