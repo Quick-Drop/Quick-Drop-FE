8bfc4c2f15ee823a70e1fca6d7f838cbbcf3ac83
Chanjoo Park <cjangel0503@gmail.com>
2024-02-13 21:12:36 +0900

    [FE-22]fix:setState.dart null할당 에러수정(변수초기화)

--- lib/userState.dart
+++ lib/userState.dart
@@ -1,12 +1,12 @@
 // userState.dart
 class UserState {
-  static int? currentUserId; // 사용자 ID를 저장할 변수
+  static int currentUserId = 0; // 사용자 ID를 저장할 변수
 
   static void setCurrentUserId(int id) {
     currentUserId = id; // 사용자 ID 설정
   }
 
-  static int? getCurrentUserId() {
+  static int getCurrentUserId() {
     return currentUserId; // 저장된 사용자 ID 반환
   }
 }
